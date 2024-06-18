f8bb57063daca000ff3ca2e1841148b0b9514f07
Chris Lauretano <1905121+lauretano@users.noreply.github.com>
2024-06-18 10:22:28 -0400

    adding a config to allow apple-bce to load early enough for LUKS unlock

---
+++ config/scripts/apple-bce-load-early.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# 2024 kansei
+# simple script to load the modules required to use your keyboard for LUKS unlock on T2 Atomic
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+
+echo 'T2-Atomic: Applying Configuration: Load apple-bce Early for LUKS Unlock'
+
+# we need to set rpm-ostree to regenerate initramfs (believe this is needed every rpm-ostree upgrade), 
+#  but building the image on github actions? hmm
+
+rpm-ostree initramfs --enable --arg=-I --arg=/etc/dracut.conf.d/t2-bce.conf
