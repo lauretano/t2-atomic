8da21c1b43dc36c6e360385ac077190b7d329a71
Chris Lauretano <1905121+lauretano@users.noreply.github.com>
2024-06-20 10:36:13 -0400

    misc: style update, and prepping for kernel arg startup script

--- config/scripts/t2-kernel.sh
+++ config/scripts/t2-kernel.sh
@@ -5,6 +5,14 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
+echo 'T2-Atomic: Kernel Pre-Install: Installing Kernel...'
+
 rpm-ostree cliwrap install-to-root / && \
     rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:sharpenedblade:t2linux kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-    #rpm-ostree kargs --append-if-missing=intel_iommu=on --append-if-missing=iommu=pt --append-if-missing=mem_sleep_default=s2idle
+  
+
+echo 'T2-Atomic: Kernel Post-Install: on bare betal, kernel arguments for thunderbolt, iommu, and S2idle will be applied by a startup script.'
+#these commands need to be run on the bare metal so put them in a script that runs at startup
+#rpm-ostree kargs --append-if-missing=intel_iommu=on --append-if-missing=iommu=pt --append-if-missing=mem_sleep_default=s2idle
+#enable initramfs regeneration via rpm-ostree, 
+#rpm-ostree initramfs --enable --arg=-I --arg=/etc/dracut.conf.d/t2-bce.conf
