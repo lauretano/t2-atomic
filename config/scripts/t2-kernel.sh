30ba765c2eff581f122d808c64dde061b9825690
Chris <1905121+lauretano@users.noreply.github.com>
2024-08-06 10:32:57 -0400

    feat: updated t2-kernel.sh for additional packages (wifi things)
    overriding fedora wpa_supplicant with t2linux version

--- config/scripts/t2-kernel.sh
+++ config/scripts/t2-kernel.sh
@@ -11,7 +11,8 @@ rpm-ostree cliwrap install-to-root / && \
     rpm-ostree override replace --experimental \
     --from repo=copr:copr.fedorainfracloud.org:sharpenedblade:t2linux \
     kernel kernel-core \
-    kernel-modules kernel-modules-core kernel-modules-extra
+    kernel-modules kernel-modules-core kernel-modules-extra \
+    wpa_supplicant
   
 
 echo 'T2-Atomic: Kernel Post-Install: on bare betal, kernel arguments for thunderbolt, iommu, and S2idle will be applied by a startup script.'
