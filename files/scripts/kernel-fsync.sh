45dafe97737a3e17adbd6b47f248b77836da7dfb
Chris Lauretano <1905121+lauretano@users.noreply.github.com>
2024-09-08 22:14:57 -0400

    feat: containerfile snippet and installer script for fsync from ublue

--- files/scripts/kernel-fsync.sh
+++ files/scripts/kernel-fsync.sh
@@ -5,13 +5,9 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-echo 'T2-Atomic: Fsync Kernel Pre-Install: Installing Fsync Kernel...'
+rpm-ostree cliwrap install-to-root / && \
 
-rpm-ostree cliwrap install-to-root / && rpm-ostree override replace --experimental --freeze \
-    --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-fsync' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-
-echo 'T2-Atomic: Fsync Kernel Post-Install: on bare betal, kernel arguments for thunderbolt, iommu, and S2idle will be applied by a startup script.'
-#these commands need to be run on the bare metal so put them in a script that runs at startup
-#rpm-ostree kargs --append-if-missing=intel_iommu=on --append-if-missing=iommu=pt --append-if-missing=mem_sleep_default=s2idle
-#enable initramfs regeneration via rpm-ostree, 
-#rpm-ostree initramfs --enable --arg=-I --arg=/etc/dracut.conf.d/t2-bce.conf
+rpm-ostree override replace --experimental \
+        /tmp/kernel-rpms/kernel-[0-9]*.rpm \
+        /tmp/kernel-rpms/kernel-core-*.rpm \
+        /tmp/kernel-rpms/kernel-modules-*.rpm
