84785d9f26895eaa57fdf8a3c7b084eb58cd7201
Chris Lauretano <1905121+lauretano@users.noreply.github.com>
2024-09-12 13:23:56 -0400

    fix: installing kernel-tools and kernel-tools-libs (so all kernel packages are from fsync copr)

--- files/scripts/kernel-fsync.sh
+++ files/scripts/kernel-fsync.sh
@@ -10,4 +10,6 @@ rpm-ostree cliwrap install-to-root / && \
 rpm-ostree override replace --experimental \
         /tmp/kernel-rpms/kernel-[0-9]*.rpm \
         /tmp/kernel-rpms/kernel-core-*.rpm \
-        /tmp/kernel-rpms/kernel-modules-*.rpm
+        /tmp/kernel-rpms/kernel-modules-*.rpm \
+        /tmp/kernel-rpms/kernel-tools-[0-9]*.rpm \
+        /tmp/kernel-rpms/kernel-tools-libs.rpm
